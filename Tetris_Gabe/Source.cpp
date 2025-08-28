a4a322472286240fc0e5462964022f88371e6447
E <ghaderimasoud568@gmail.com>
2025-08-28 11:01:33 -0500

    Update Source.cpp

--- Tetris_Gabe/Source.cpp
+++ Tetris_Gabe/Source.cpp
@@ -11,6 +11,11 @@ tetromino[0].append(L"..X.");
 tetromino[0].append(L"..X.");
 tetromino[0].append(L"..X.");
 
+tetromino[0].append(L"..X.");
+tetromino[0].append(L"..X.");
+tetromino[0].append(L"..X.");
+tetromino[0].append(L"..X.");
 
+	
 	return 0;
 }
