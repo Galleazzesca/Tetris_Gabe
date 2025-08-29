2169a3098a919a01691081f9e19f4be604ab9a8a
E <ghaderimasoud568@gmail.com>
2025-08-28 19:49:31 -0500

    Update Source.cpp

--- Tetris_Gabe/Source.cpp
+++ Tetris_Gabe/Source.cpp
@@ -57,18 +57,10 @@ tetromino[6].append(L".XX.");
 tetromino[6].append(L".X..");
 tetromino[6].append(L".X..");
 
-	
-	return 0;
-}
+pFeild =  new unsigned char[nFeildWidth*nFeildHieght]; // Create play feild buffer
+for (int x = 0; x < nFeildWitdth; x++) // Board Boundary
+    for (int y = 0; y < nFeildHeight; y++)
+		pFeild[y*nFeildWidth + x] = (x == 0 || x == nFeildWidth - 1 || y == nFeildHeight - 1) ? 9 : 0;
 
-int Rotate(int px, int py, int r)
-{
-	switch (r % 4)
-	{
-	case 0: return py * 4 + px;             // 0 degrees
-	case 1: return 12 + py - (px * 4);      // 90 degrees
-	case 2: return 15 - (py * 4) - px;      // 180 degrees
-	case 3: return 3 - py + (px * 4);       // 270 degrees
-	}
-	return 0;
+return 0;
 }
