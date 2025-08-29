294383f6eab4c78be1803a90687128587243dbe3
E <ghaderimasoud568@gmail.com>
2025-08-28 19:53:11 -0500

    Update Source.cpp
    fixed spacing

--- Tetris_Gabe/Source.cpp
+++ Tetris_Gabe/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+#include <Windows.h>
+
 wsrtring tetromino[7];
 int nFeildWidth = 12;
 int nFeildHeight = 18;
@@ -13,7 +15,7 @@ int Rotate(int px, int py, int r)
     case 0: return py * 4 + px;         // 0 degrees 
     case 1: return 12 + py - (px * 4);  // 90 degrees
     case 2: return 15 - (py * 4) - px;  // 180 degrees
-    case 3: return 3 - py + (px * 4);   // 270 degrees
+	case 3: return 3 - py + (px * 4);   // 270 degrees
     }
     return 0;
 }
@@ -22,45 +24,45 @@ int Rotate(int px, int py, int r)
 int main()
 {
  // create assests
-tetromino[0].append(L"..X.");
-tetromino[0].append(L"..X.");
-tetromino[0].append(L"..X.");
-tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
 
-tetromino[1].append(L"..X.");
-tetromino[1].append(L".XX.");
-tetromino[1].append(L".X..");
-tetromino[1].append(L"....");
+	tetromino[1].append(L"..X.");
+	tetromino[1].append(L".XX.");
+	tetromino[1].append(L".X..");
+	tetromino[1].append(L"....");
 
-tetromino[2].append(L".X..");
-tetromino[2].append(L".XX.");
-tetromino[2].append(L"..X.");
-tetromino[2].append(L"....");
+	tetromino[2].append(L".X..");
+	tetromino[2].append(L".XX.");
+	tetromino[2].append(L"..X.");
+	tetromino[2].append(L"....");
 
-tetromino[3].append(L"....");
-tetromino[3].append(L".XX.");
-tetromino[3].append(L".XX.");
-tetromino[3].append(L"....");
+	tetromino[3].append(L"....");
+	tetromino[3].append(L".XX.");
+	tetromino[3].append(L".XX.");
+	tetromino[3].append(L"....");
 
-tetromino[4].append(L"..X.");
-tetromino[4].append(L".XX.");
-tetromino[4].append(L"..X.");
-tetromino[4].append(L"....");
+	tetromino[4].append(L"..X.");
+	tetromino[4].append(L".XX.");
+	tetromino[4].append(L"..X.");
+	tetromino[4].append(L"....");
 
-tetromino[5].append(L"....");
-tetromino[5].append(L".XX.");
-tetromino[5].append(L"..X.");
-tetromino[5].append(L"..X.");
+	tetromino[5].append(L"....");
+	tetromino[5].append(L".XX.");
+	tetromino[5].append(L"..X.");
+	tetromino[5].append(L"..X.");
 
-tetromino[6].append(L"....");
-tetromino[6].append(L".XX.");
-tetromino[6].append(L".X..");
-tetromino[6].append(L".X..");
+	tetromino[6].append(L"....");
+	tetromino[6].append(L".XX.");
+	tetromino[6].append(L".X..");
+	tetromino[6].append(L".X..");
 
-pFeild =  new unsigned char[nFeildWidth*nFeildHieght]; // Create play feild buffer
-for (int x = 0; x < nFeildWitdth; x++) // Board Boundary
-    for (int y = 0; y < nFeildHeight; y++)
-		pFeild[y*nFeildWidth + x] = (x == 0 || x == nFeildWidth - 1 || y == nFeildHeight - 1) ? 9 : 0;
+	pFeild =  new unsigned char[nFeildWidth*nFeildHieght]; // Create play feild buffer
+	 for (int x = 0; x < nFeildWitdth; x++) // Board Boundary
+		 for (int y = 0; y < nFeildHeight; y++)
+			 pFeild[y*nFeildWidth + x] = (x == 0 || x == nFeildWidth - 1 || y == nFeildHeight - 1) ? 9 : 0;
 
-return 0;
+	return 0;
 }
