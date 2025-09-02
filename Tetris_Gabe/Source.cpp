d253051fac82e63721fdbefee9cb2ac36db829cf
Gabriel Cain <gcain3@email.tjc.edu>
2025-09-02 09:04:52 -0500

    Debug

--- Tetris_Gabe/Source.cpp
+++ Tetris_Gabe/Source.cpp
@@ -3,32 +3,32 @@ using namespace std;
 
 #include <Windows.h>
 
-wsrtring tetromino[7];
+wstring tetromino[7];
 int nFieldWidth = 12;
 int nFieldHeight = 18;
-unsigned char *pFeild = nullptr;
+unsigned char* pField = nullptr;
 
 int nScreenWidth = 80;          // Console Screen Size X (Columns)
 int nScreenHeight = 30;         // Console Screen Size Y (rows)
 
 int Rotate(int px, int py, int r)
 {
-    Switch (r % 4)
-    {
+	switch(r % 4)
+	{
     case 0: return py * 4 + px;         // 0 degrees 
-    case 1: return 12 + py - (px * 4);  // 90 degrees
-    case 2: return 15 - (py * 4) - px;  // 180 degrees
+	case 1: return 12 + py - (px * 4);  // 90 degrees
+	case 2: return 15 - (py * 4) - px;  // 180 degrees
 	case 3: return 3 - py + (px * 4);   // 270 degrees
-    }
-    return 0;
+	}
+	return 0;
 }
 
 bool DoesPeiceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
 {
-    for (int px = 0; px < 4; px++)
+	for (int px = 0; px < 4; px++)
 		for (int py = 0; py < 4; py++)
 		{
-            // Get index into peice
+			// Get index into piece
 			int pi = Rotate(px, py, nRotation);
 
 			// Get index into field
@@ -36,21 +36,21 @@ bool DoesPeiceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
 
 			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
 			{
-				if (nPosY + py) >= 0 && nPosY + py < nFieldHeight
+				if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
 				{
-		            if (tetromino[nTetromino][pi] == L'X' && pField[fi] != 0)
-					    return false; // fail on first hit
+					if (tetromino[nTetromino][pi] == L'X' && pField[fi] != 0)
+						return false; // fail on first hit
 				}
 			}
 		}
 
-	
-    return true;
+
+	return true;
 }
 
 int main()
 {
-    // create assests
+	// create assests
 	tetromino[0].append(L"..X.");
 	tetromino[0].append(L"..X.");
 	tetromino[0].append(L"..X.");
@@ -86,20 +86,20 @@ int main()
 	tetromino[6].append(L".X..");
 	tetromino[6].append(L".X..");
 
-	pField =  new unsigned char[nFieldWidth*nFieldHeight]; // Create play field buffer
-	 for (int x = 0; x < nFieldWidtth; x++) // Board Boundary
-		 for (int y = 0; y < nFieldHeight; y++)
-			 pFeild[y*nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
+	pField = new unsigned char[nFieldWidth * nFieldHeight]; // Create play field buffer
+	for (int x = 0; x < nFieldWidth; x++) // Board Boundary
+		for (int y = 0; y < nFieldHeight; y++)
+			pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
 
-    wchar_t *screen = new wchar_t[nScreenWitdh*nScreenHeight];
-    for (int i = 0; i < nScreenWidth*nScreenHeight; i++) screen[i] = L' ';
-    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-    SetConsoleActiveScreenBuffer(hCosole);
-    DWORD dwBytesWritten = 0;
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
 
 
 
-bool bGameOver = false;
+	bool bGameOver = false;
 
 
 
@@ -108,21 +108,22 @@ bool bGameOver = false;
 		// GAME TIME ===================================
 
 
-        // INPUT =======================================
-		
+		// INPUT =======================================
 
-        // GAME LOGIC ==================================
-	
 
-        // RENDER OUTPUT ===============================
+		// GAME LOGIC ==================================
+
+
+		// RENDER OUTPUT ===============================
 
 		// Draw Field
 		for (int x = 0; x < nFieldWidth; x++)
 			for (int y = 0; y < nFieldHeight; y++)
-				screen[(y + 2)*nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y*nFieldWidth + x]];
-		
-		// Dsiplay Frame
-		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0
+				screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
+
+		// Display Frame
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+			}
+
+		return 0;
 	}
-    return 0;
-}
